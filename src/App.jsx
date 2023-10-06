12444ed0ab828d82213e57cc9486369751bccd9a
HumaionKobir1 <humaionkobir341@gmail.com>
2023-10-06 07:29:53 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <h1 className="text-6xl text-center text-green-600 mt-10 font-bold underline">
+      Smart tour & safe journey!
+    </h1>
+  )
+}
+
+export default App
