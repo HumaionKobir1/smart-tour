b300674c6fb7eace8685b46532f36c8336155a96
Your Name <you@example.com>
2023-10-16 08:13:38 +0600

    create navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,8 @@
-
 const Home = () => {
-    return(
-        <div>
-            <h1 className="text-6xl text-center text-green-600 mt-10 font-bold underline">Smart tour & safe journey!</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      {/* <h1 className="text-6xl text-center text-green-600 mt-10 font-bold underline">Smart tour & safe journey!</h1> */}
+    </div>
+  );
+};
 export default Home;
